feda85a88d1b84a2620bae0bc1f336780a0d775f
Dhameemul Ansari <110928186+Dhameem99@users.noreply.github.com>
2023-02-28 00:52:15 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -16,7 +16,7 @@ function About() {
          <p className='about__description'>Hello! I'm Dhameemul Ansari I, a passionate Full Stack Developement. I develop web applications. My core skill is based on
 JavaScript and I love to do most of the things using JavaScript. I love to make the web more open to the world. I have graduated with a bachelor's degree in Computer Application from Nehru Arts And Science College at
 Coimbatore, TamilNadu in 2022. I am available for any kind of job opportunity that suits my interests.</p>
-         <a  href="https://drive.google.com/file/d/1_0d4U8zXoW_xlDQbrvWQOpjVETnvytCf/view?usp=share_link" 
+         <a  href="https://drive.google.com/file/d/1Gp_ouy2NZMLMnX0ets7SMmQRYeQc723j/view?usp=share_link" 
          className="button button__cv button--flex" target="_blank">Download CV</a>
         </div>
         </div>
